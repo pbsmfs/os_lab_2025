2b6bb3676e100e0cf2465052ac4e2ed0ce27520a
pbsmfs <fsmart2006@gmail.com>
2025-09-23 17:00:06 +0300

    lab2 fix: added modified files

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,21 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+    int length = strlen(str);
+    
+    char *start = str;
+    char *end = str + length - 1;
+    
+    while (start < end)
+    {
+        char temp = *start;
+        *start = *end;
+        *end = temp;
+        
+        start++;
+        end--;
+    }
 }
 
