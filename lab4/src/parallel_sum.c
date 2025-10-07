6a963d01c83522b9651cf32dca682b803dcdcd8e
TWIT <fsmart2006@gmail.com>
2025-10-07 18:39:05 +0000

    lab4 done

--- lab4/src/parallel_sum.c
+++ lab4/src/parallel_sum.c
@@ -1,8 +1,10 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+#include <time.h>
 #include <pthread.h>
+#include "utils.h"
 
 struct SumArgs {
   int *array;
@@ -12,7 +14,9 @@ struct SumArgs {
 
 int Sum(const struct SumArgs *args) {
   int sum = 0;
-  // TODO: your code here 
+  for (int i = args->begin; i < args->end; i++) {
+    sum += args->array[i];
+  }
   return sum;
 }
 
@@ -22,30 +26,50 @@ void *ThreadSum(void *args) {
 }
 
 int main(int argc, char **argv) {
-  /*
-   *  TODO:
-   *  threads_num by command line arguments
-   *  array_size by command line arguments
-   *	seed by command line arguments
-   */
-
   uint32_t threads_num = 0;
   uint32_t array_size = 0;
   uint32_t seed = 0;
-  pthread_t threads[threads_num];
 
-  /*
-   * TODO:
-   * your code here
-   * Generate array here
-   */
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "--threads_num") == 0 && i + 1 < argc) {
+      threads_num = atoi(argv[++i]);
+    } else if (strcmp(argv[i], "--array_size") == 0 && i + 1 < argc) {
+      array_size = atoi(argv[++i]);
+    } else if (strcmp(argv[i], "--seed") == 0 && i + 1 < argc) {
+      seed = atoi(argv[++i]);
+    }
+  }
+
+  if (threads_num == 0 || array_size == 0) {
+    printf("Usage: %s --threads_num <num> --seed <num> --array_size <num>\n", argv[0]);
+    return 1;
+  }
 
   int *array = malloc(sizeof(int) * array_size);
+  if (array == NULL) {
+    printf("Error: Memory allocation failed!\n");
+    return 1;
+  }
+  
+  GenerateArray(array, array_size, seed);
 
+  pthread_t threads[threads_num];
   struct SumArgs args[threads_num];
+  
+  int chunk_size = array_size / threads_num;
   for (uint32_t i = 0; i < threads_num; i++) {
-    if (pthread_create(&threads[i], NULL, ThreadSum, (void *)&args)) {
+    args[i].array = array;
+    args[i].begin = i * chunk_size;
+    args[i].end = (i == threads_num - 1) ? array_size : (i + 1) * chunk_size;
+  }
+
+  struct timespec start, end;
+  clock_gettime(CLOCK_MONOTONIC, &start);
+
+  for (uint32_t i = 0; i < threads_num; i++) {
+    if (pthread_create(&threads[i], NULL, ThreadSum, (void *)&args[i])) {
       printf("Error: pthread_create failed!\n");
+      free(array);
       return 1;
     }
   }
@@ -57,7 +81,13 @@ int main(int argc, char **argv) {
     total_sum += sum;
   }
 
+  clock_gettime(CLOCK_MONOTONIC, &end);
+
+  double time_taken = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+
   free(array);
   printf("Total: %d\n", total_sum);
+  printf("Time: %.6f seconds\n", time_taken);
+  
   return 0;
 }
